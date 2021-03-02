f94f06a719579cf0a2fc3c7aab95baacd2de271a
yangmr <759168442@qq.com>
2021-03-02 21:40:38 +0800

    引入iview组件库

--- clouddisk/src/views/Home.vue
+++ clouddisk/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Button type="primary">Primary</Button>
+    <Button type="dashed">Dashed</Button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+
   }
 };
 </script>
