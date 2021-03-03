a147188252358c08dc873027174618a0f3620f98
yangmr <759168442@qq.com>
2021-03-03 14:38:20 +0800

    pc端登录页开发(一)

--- clouddisk/src/views/login.vue
+++ clouddisk/src/views/login.vue
@@ -1,11 +1,28 @@
 <template>
-	<div>
-		我是登录组件
+	<div class="login-wrapper d-flex  justify-content-center">
+		<div class=" bg-white login-box rounded p-3">
+			<div class="d-flex flex-column align-items-center justify-content-center">
+				<div class="d-flex align-items-center">
+					<Icon type="md-briefcase" size="55" color="#2db7f5" />
+					<h3 class="text-muted ml-2">企业网盘</h3>
+				</div>
+				<span class="text-muted small mt-3">企业级网盘解决方案</span>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
 </script>
 
-<style>
+<style scoped="scoped">
+.login-wrapper{
+	height : 100%;
+	background-color: #515a56;
+}
+.login-box{
+	width: 380px;
+	margin-top: 50px;
+	height: max-content;
+}
 </style>
