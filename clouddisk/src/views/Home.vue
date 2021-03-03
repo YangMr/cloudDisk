55adf49393cf1e92e05474845ac2c91d7cf8063f
yangmr <759168442@qq.com>
2021-03-03 14:14:12 +0800

    引入bootstrap库与创建登录组件以及配置登录组件路由

--- clouddisk/src/views/Home.vue
+++ clouddisk/src/views/Home.vue
@@ -2,6 +2,8 @@
   <div class="home">
     <Button type="primary">Primary</Button>
     <Button type="dashed">Dashed</Button>
+	
+	<span class="btn btn-danger">hello world</span>
   </div>
 </template>
 
