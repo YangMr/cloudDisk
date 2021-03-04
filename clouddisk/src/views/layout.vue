58501768c898bfbf40b6a17e339254d260a91060
yangmr <759168442@qq.com>
2021-03-04 10:20:53 +0800

    pc端全局布局开发(二) 顶部导航

--- clouddisk/src/views/layout.vue
+++ clouddisk/src/views/layout.vue
@@ -1,14 +1,27 @@
 <template>
 	<Layout class="layout">
 		<Header class="bg-white px-0">
-			 <Menu @on-select="handleSelect" mode="horizontal" theme="light" :active-name="topActive" class="d-flex align-items-center">
-					<div class="layout-logo">企业网盘</div>
-			        <MenuItem :name="index" v-for="(item,index) in topMenus" :key="index">
-			            <Icon :type="item.icon" />
-			            {{item.title}}
-			        </MenuItem>
-					<div>下拉菜单</div>
-			    </Menu>
+			<Menu @on-select="handleSelect" mode="horizontal" theme="light" :active-name="topActive" class="d-flex align-items-center">
+				<div class="layout-logo">企业网盘</div>
+				<MenuItem :name="index" v-for="(item,index) in topMenus" :key="index">
+				<Icon :type="item.icon" />
+				{{item.title}}
+				</MenuItem>
+				<div class="ml-auto mr-3">
+					<Dropdown style="margin-left: 20px" placement="bottom-end">
+						<a href="javascript:void(0)">
+							<Avatar src="https://i.loli.net/2017/08/21/599a521472424.jpg" />
+							管理员
+							<Icon type="ios-arrow-down"></Icon>
+						</a>
+						<DropdownMenu slot="list">
+							<DropdownItem>个人资料</DropdownItem>
+							<DropdownItem>修改密码</DropdownItem>
+							<DropdownItem>安全退出</DropdownItem>
+						</DropdownMenu>
+					</Dropdown>
+				</div>
+			</Menu>
 		</Header>
 		<Layout>
 			<Sider hide-trigger class="bg-light">侧边栏</Sider>
@@ -27,25 +40,24 @@
 	export default {
 		data() {
 			return {
-				topActive : 0,
-				topMenus : [
-					{
-						icon : "ios-paper",
-						title : "网盘"
+				topActive: 0,
+				topMenus: [{
+						icon: "ios-paper",
+						title: "网盘"
 					},
 					{
-						icon : "ios-people",
-						title : "分享"
+						icon: "ios-people",
+						title: "分享"
 					},
 					{
-						icon : "ios-construct",
-						title : "更多"
+						icon: "ios-construct",
+						title: "更多"
 					}
 				]
 			}
 		},
-		methods : {
-			handleSelect(e){
+		methods: {
+			handleSelect(e) {
 				console.log(e)
 				this.topActive = e;
 			}
@@ -57,10 +69,12 @@
 	.layout {
 		height: 100%;
 	}
-	.footer-left{
+
+	.footer-left {
 		width: 200px;
 	}
-	.layout-logo{
+
+	.layout-logo {
 		width: 200px;
 		text-align: center;
 		font-size: 25px;
